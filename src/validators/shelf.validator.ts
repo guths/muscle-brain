a5fc6f1f22aeb8208d4e450bf95d71d2b2294fba
Guths xD <jonathanguths@gmail.com>
2023-05-12 09:43:55 -0300

    comecando validacao shelf

--- src/validators/shelf.validator.ts
+++ src/validators/shelf.validator.ts
@@ -1,3 +1,5 @@
-export const shelfValidator = () => {
+import { check } from "express-validator"
 
+export const createShelfValidator = () => {
+  check('name').trim().not().isEmpty().withMessage('Shelf name is required')
 }
